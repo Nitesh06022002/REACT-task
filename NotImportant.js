e9c955a4687fdec66aa3638fd6f42e938746e845
Nitesh06022002 <78930743+Nitesh06022002@users.noreply.github.com>
2021-07-17 02:52:31 +0530

    Add files via upload

---
+++ NotImportant.js
@@ -0,0 +1,16 @@
+const NotImportant  = () => {
+    const handleClick = () => {
+        console.log('Not Important');
+    }
+    return (
+        <div className='Not Important'>
+            <button>
+               NOT IMPORTANT
+            </button>
+            
+        </div> 
+
+     );
+}
+ 
+export default NotImportant ;
